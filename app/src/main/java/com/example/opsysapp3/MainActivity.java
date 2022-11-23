265e56f8e982118e298d1d632f99d17346f40da8
Logan Keim <keimlogan123@gmail.com>
2022-11-22 23:51:51 -0500

    Finished the entire app

--- app/src/main/java/com/example/opsysapp3/MainActivity.java
+++ app/src/main/java/com/example/opsysapp3/MainActivity.java
@@ -22,4 +22,44 @@ public class MainActivity extends AppCompatActivity {
     {
         setContentView(R.layout.chapter_1);
     }
+
+    public void renderChap2(View view)
+    {
+        setContentView(R.layout.chapter_2);
+    }
+
+    public void renderChap3(View view)
+    {
+        setContentView(R.layout.chapter_3);
+    }
+
+    public void renderChap4(View view)
+    {
+        setContentView(R.layout.chapter_4);
+    }
+
+    public void renderChap5(View view)
+    {
+        setContentView(R.layout.chapter_5);
+    }
+
+    public void renderChap6(View view)
+    {
+        setContentView(R.layout.chapter_6);
+    }
+
+    public void renderChap7(View view)
+    {
+        setContentView(R.layout.chapter_7);
+    }
+
+    public void renderChap8(View view)
+    {
+        setContentView(R.layout.chapter_8);
+    }
+
+    public void renderChap12(View view)
+    {
+        setContentView(R.layout.chapter_12);
+    }
 }
